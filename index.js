c7b9d8a5b2f40dbd609ea6aefda4ee27dd69e7fe
Lili Rodriguez <lilimrod@gmail.com>
2020-02-13 04:35:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key){
-  delete object.key
+  
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  
+  delete object.key
 }

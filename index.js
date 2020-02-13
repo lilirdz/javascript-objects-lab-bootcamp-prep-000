3bd88e3289ab0e0693997b9199046a0a4561fac4
Lili Rodriguez <lilimrod@gmail.com>
2020-02-13 04:28:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var recipes = {};
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+  
+}
+
+
 function deleteFromObjectByKey(object, key){}
+
+
+function destructivelyDeleteFromObjectByKey(object, key) {}
